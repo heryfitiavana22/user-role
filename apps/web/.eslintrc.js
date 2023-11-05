9a496ca87c319daadbacf248abefe44231c84a22
heryfitiavana22 <hheryfitiavana@gmail.com>
2023-11-05 10:33:53 +0300

    fix: lint web

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
-  root: true,
-  extends: ["custom"],
-};
+    root: true,
+    extends: ["custom"],
+    rules: {
+        "react/no-unescaped-entities": "off",
+        "@next/next/no-page-custom-font": "off",
+    },
+}
